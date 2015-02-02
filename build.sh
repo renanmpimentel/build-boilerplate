58eb5f422cd86627f0b814e63c22b9e6f27d9954
Renan Pimentel <renan.pimentel@gmail.com>
2015-02-01 23:26:07 -0200

    Build init

--- build.sh
+++ build.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+DIST="build"
+
+IGNORE=(
+    "assets/styles/style.css"
+    "assets/scripts/main.js"
+)
+
+STYLE="assets/styles"
+SCRIPT="assets/scripts"
+
+# JS - http://javascript-minifier.com/examples
+echo -e "Minifier JS"
+curl -X POST -s --data-urlencode "input@${SCRIPT}/main.js" http://javascript-minifier.com/raw > ${SCRIPT}/main.min.js
+
+# CSS - http://cssminifier.com/examples
+echo -e "Minifier CSS"
+curl -X POST -s --data-urlencode "input@${STYLE}/style.css" http://cssminifier.com/raw > ${STYLE}/style.min.css
+
+if [[ ! -d ${DIST} ]]; then
+    mkdir ${DIST}
+fi
+
+cp ./* ${DIST}
+rm -rf ${IGNORE}
+
+echo -e "Build complete"
