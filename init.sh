2291ab975a7e51edbe29acfd7ccb68824116beb5
Renan Pimentel <renan.pimentel@gmail.com>
2015-02-01 23:34:46 -0200

    Script

--- init.sh
+++ init.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+FILES="index.html assets/styles/style.css assets/scripts/main.js"
+PATHS="assets/scripts assets/styles assets/images assets/fonts"
+
+
+echo -n "Nome do projeto: "
+read PROJECT
+
+echo -e "Criando Estrutura"
+
+mkdir -p $PROJECT && cd $_
+mkdir -p $PATHS
+touch $FILES
+
+# Mensagem final
+echo -e "\n Projeto $PROJECT criado!\n"
